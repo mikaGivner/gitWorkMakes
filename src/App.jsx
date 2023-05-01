8823e8e16a634c791db06bbf53ab6e4d4db2bd9c
Toni Zoaretz <tonizoaretz1@gmail.com>
2023-05-01 17:13:42 +0300

    toni

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [joke, setJoke] = useState([]);
+
+  const getJokesFuncion = () => {
+    let options = {
+      method: "GET",
+      headers: {
+        "x-api-key": "dDngk3K69nspwV4M2GNXFQ==yMoXWr09NuNxkpyZ",
+      },
+    };
+
+    let url = "https://api.api-ninjas.com/v1/jokes?limit=1";
+
+    fetch(url, options)
+      .then((res) => res.json()) // parse response as JSON
+      .then((data) => {
+        console.log(data);
+        setJoke(data);
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+
+    console.log(joke);
+  };
+
+  useEffect(() => {
+    getJokesFuncion();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Jokes App!</h1>
+      <p>The Joke is...</p>
+      <p>
+        {joke?.map((joke, index) => {
+          return (
+            <p key={index}>
+              <h2>{joke.joke}</h2>
+            </p>
+          );
+        })}
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
