4f9b3fa2fd8889a7e418030851a772567058c91d
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-05-02 11:45:55 +0300

    git diff 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,42 @@
-
-import { useState, useEffect } from 'react'
-import { AiOutlineLike } from 'react-icons/ai'
-import { AiFillLike } from 'react-icons/ai'
-import { AiOutlineDislike } from 'react-icons/ai'
-import { AiFillDislike } from 'react-icons/ai'
+import { useState, useEffect } from "react";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiFillLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
 import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs";
 
-import './App.css'
-import './styles/like.css'
-
-
+import "./App.css";
+import "./styles/like.css";
 
 function App() {
   const [joke, setJoke] = useState([]);
   const [openDiv, setOpenDiv] = useState(false);
-  const [likes, setLikes] = useState(0)
-  const [isLiked, setIsLiked] = useState(false)
-  const [dislikes, setDislikes] = useState(0)
-  const [isDisliked, setIsDisliked] = useState(false)
-
+  const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+  const [dislikes, setDislikes] = useState(0);
+  const [isDisliked, setIsDisliked] = useState(false);
 
   const likeClick = () => {
-
     if (!isLiked) {
-      setLikes(likes + 1)
-      setDislikes(0)
-      setIsLiked(true)
-      setIsDisliked(false)
+      setLikes(likes + 1);
+      setDislikes(0);
+      setIsLiked(true);
+      setIsDisliked(false);
     }
-
-
-  }
+  };
 
   const dislikeClick = () => {
     if (!isDisliked) {
-      setDislikes(dislikes + 1)
-      setLikes(0)
-      setIsDisliked(true)
-      setIsLiked(false)
+      setDislikes(dislikes + 1);
+      setLikes(0);
+      setIsDisliked(true);
+      setIsLiked(false);
     }
-  }
+  };
 
   useEffect(() => {
     getJokesFunction();
   }, []);
-
 
   const getJokesFunction = () => {
     let options = {
@@ -69,8 +61,6 @@ function App() {
     console.log(joke);
   };
 
-
-
   const getAJoke = () => {
     setOpenDiv(!openDiv);
     console.log(`Now it's ${openDiv}`);
@@ -90,6 +80,8 @@ function App() {
         <div className="fullContainer">
           <h1>Jokes App!</h1>
           <p>The Joke is...</p>
+          <p>This is title for diff command 1 </p>
+
           <p>
             {joke?.map((joke, index) => {
               return (
@@ -100,19 +92,23 @@ function App() {
             })}
           </p>
 
-          <div className='likes-container'>
-            <div className='like'>
-              <div className='like-logo'> {isLiked ? <AiFillLike /> : <AiOutlineLike />} </div>
-              <div className='like-counter' >{likes}</div>
-              <div className='dislike-logo'>{isDisliked ? <AiFillDislike /> : <AiOutlineDislike />}</div>
-              <div className='dislike-counter'>{dislikes}</div>
+          <div className="likes-container">
+            <div className="like">
+              <div className="like-logo">
+                {" "}
+                {isLiked ? <AiFillLike /> : <AiOutlineLike />}{" "}
+              </div>
+              <div className="like-counter">{likes}</div>
+              <div className="dislike-logo">
+                {isDisliked ? <AiFillDislike /> : <AiOutlineDislike />}
+              </div>
+              <div className="dislike-counter">{dislikes}</div>
             </div>
-            <div className='buttons'>
+            <div className="buttons">
               <button onClick={() => likeClick()}>Like</button>
               <button onClick={() => dislikeClick()}>Dislike</button>
             </div>
-
-          </div >
+          </div>
           <BsArrowUpCircle
             className="icon down"
             onMouseEnter={getAJoke}
@@ -122,8 +118,7 @@ function App() {
       )}
       <div className="jokeTitle">Have a nice day</div>
     </>
-  ); 
-
+  );
 }
 
-export default App
+export default App;
